f48d28086b5bf4d52f84e2b52a027f4367567b7a
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2022-12-31 06:57:27 +0530

    Add files via upload

---
+++ updatedgithubreadme.c
@@ -0,0 +1,92 @@
+<p align="center">
+  <img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&text=Hello!&height=100&section=header"/>
+</p>
+
+<h1 align="center">
+  Let's Connect and have a Chat!💬
+</h1>
+
+<p align="center">
+<a href="https://www.linkedin.com/in/kartik-jain-473ab81b2/">
+  <img height="50" src="https://user-images.githubusercontent.com/46517096/166973395-19676cd8-f8ec-4abf-83ff-da8243505b82.png"/>
+</a>
+<a href="https://twitter.com/kartikk_26">
+  <img height="50" src="https://user-images.githubusercontent.com/46517096/166974271-91dfa250-d70b-4cb9-8707-f1bda1b708c3.png"/>
+</a>
+<a href="https://instagram.com/kartik__j26?igshid=YmMyMTA2M2Y=">
+  <img height="50" src="https://user-images.githubusercontent.com/46517096/166974368-9798f39f-1f46-499c-b14e-81f0a3f83a06.png"/>
+</a>
+</p>
+
+<p align="center">
+  <img src= "https://i.giphy.com/media/q217GUnfKAmJlFcjBX/giphy.webp">
+</p>
+
+---
+
+<h2> 👨🏻‍💻 A Little Bit About Me and My Interests</h2>
+
+```yaml
+name: Kartik Jain
+located_in: Udaipur, Rajasthan
+current_course: Btech CSE
+education:
+  [
+    "Self-Taught Developer and Designer",
+    "Enrolled With CSE Course",
+  ]
+
+fields_of_interests:
+  [
+    "Web Development",
+    "Data Science",
+    "Machine Learning",
+    "Front End",
+    "Back End",
+  ]
+technical_background:
+  [
+    "Digital 101 Certified For ML",
+    "Enrolled With IIT Madras For Python Course",
+  ]
+  
+currently_learning: ["Python,C++"]
+2023 Goals: ["Create 20+ Project On C++ & Python."]
+hobbies: ["Cricket", "MMA", "Music"]
+```
+
+---  
+  
+<h2> 🚀 Some Tools I Have Used and Learned</h2>
+<p align="left">
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" alt="vscode" width="45" height="45"/>
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg" alt="python" width="45" height="45" />
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" width="45" height="45"/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html" width="45" height="45"/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" width="45" height="45"/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" alt="git" width="45" height="45" />
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="git" width="45" height="45" />
+          
+
+# 📈 GitHub Stats:
+
+<a href="https://github.com/Kartikk-26">
+  <img height="180em" src="https://github-readme-streak-stats.herokuapp.com/?user=Kartikk-26&theme=noctis_minimus&show_icons=true" />
+  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Kartikk-26&theme=noctis_minimus&layout=compact" />
+</a>
+
+<p align="left">
+  <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--E4gnEuy_--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/233m04x0r0lv60payria.gif"/>
+</p>
+
+### ✍️ Random Dev Quote
+![](https://quotes-github-readme.vercel.app/api?type=vetical&theme=gruvbox)
+
+
+
+
+
+---
+[![](https://visitcount.itsvg.in/api?id=Kartikk-26&icon=1&color=12)](https://visitcount.itsvg.in)
+
+<!-- Proudly created with GPRM ( https://gprm.itsvg.in ) -->
